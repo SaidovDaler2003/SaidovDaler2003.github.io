c574081ae1f1ec19d03b8809cfba200a843cce6e
Daler Saidov <dalerchess@mail.ru>
2021-07-17 17:16:25 +0300

    Initil

--- css7/index.js
+++
@@ -1,9 +0,0 @@
-toggle_icon.onclick = (e) => {
-  nav.classList.remove('nav_anim-close');
-  nav.classList.add('nav_anim-open');
-};
-
-close_icon.onclick = (e) => {
-  nav.classList.remove('nav_anim-open');
-  nav.classList.add('nav_anim-close');
-};
--- resume/js/toggleQues.js
+++
@@ -1,7 +0,0 @@
-const ques = document.getElementsByClassName('ques__que');
-
-for (let que of ques) {
-  que.addEventListener('click', (e) => {
-    que.classList.toggle('ques__que--closed');
-  }); 
-}
